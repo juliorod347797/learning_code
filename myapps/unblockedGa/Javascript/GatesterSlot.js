7528bce2fb456972f68084e7938aa7eb13488511
Lenddy <leme2003@gmail.com>
2023-11-17 19:12:18 -0600

    from julio

--- myapps/unblockedGa/Javascript/GatesterSlot.js
+++ myapps/unblockedGa/Javascript/GatesterSlot.js
@@ -1,120 +1,143 @@
 (function () {
-  const items = [
-      '🍭',
-      '❌',
-      '⛄️',
-      '🦄',
-      '🍌',
-      '💩',
-      '👻',
-      '😻',
-      '💵',
-      '🤡',
-      '🦖',
-      '🍎',
-      '😂',
-      '🖕',
-  ];
-  const doors = document.querySelectorAll('.door');
-  const coinSound = new Audio("../music/coininsert.mp3");
-  const spinSound = new Audio("../music/wheelspinslot.mp3");
+	// Array of items that will appear on the slot machine doors
+	const items = [
+		"🍭",
+		"❌",
+		"⛄️",
+		"🦄",
+		"🍌",
+		"💩",
+		"👻",
+		"😻",
+		"💵",
+		"🤡",
+		"🦖",
+		"🍎",
+		"😂",
+		"🖕",
+	];
+	// Selecting all elements with the class 'door' and storing them in 'doors'
+	const doors = document.querySelectorAll(".door");
 
-  document.querySelector('#spinner').addEventListener('click', spin);
-  document.querySelector('#reseter').addEventListener('click', init);
+	// Loading sounds for the slot machine
+	const coinSound = new Audio("../music/coininsert.mp3");
+	const spinSound = new Audio("../music/wheelspinslot.mp3");
 
-  function init(firstInit = true, groups = 1, duration = 1) {
-      for (const door of doors) {
-          if (firstInit) {
-              door.dataset.spinned = '0';
-          } else if (door.dataset.spinned === '1') {
-              const boxes = door.querySelector('.boxes');
-              slowDownAnimation(boxes, duration);
-              return;
-          }
+	// Event listener for the 'spinner' button, triggering the 'spin' function
+	document.querySelector("#spinner").addEventListener("click", spin);
 
-          const boxes = door.querySelector('.boxes');
-          const boxesClone = boxes.cloneNode(false);
-          const pool = ['❓'];
+	// Event listener for the 'reseter' button, calling the 'init' function with default values
+	document.querySelector("#reseter").addEventListener("click", init);
 
-          if (!firstInit) {
-              const arr = [];
-              for (let n = 0; n < (groups > 0 ? groups : 1); n++) {
-                  arr.push(...items);
-              }
-              pool.push(...shuffle(arr));
+	// Initializing the slot machine
+	function init(firstInit = true, groups = 1, duration = 1) {
+		for (const door of doors) {
+			// Resetting or initializing the 'spinned' data attribute for each door
+			if (firstInit) {
+				door.dataset.spinned = "0";
+			} else if (door.dataset.spinned === "1") {
+				const boxes = door.querySelector(".boxes");
+				// ! this is commented by lenddy
+				// slowDownAnimation(boxes, duration);
+				return;
+			}
 
-              boxesClone.addEventListener(
-                  'transitionstart',
-                  function () {
-                      door.dataset.spinned = '1';
-                      this.querySelectorAll('.box').forEach((box) => {
-                          box.style.filter = 'blur(1px)';
-                      });
-                  },
-                  { once: true }
-              );
+			// Creating a clone of the door's boxes for animation
+			const boxes = door.querySelector(".boxes");
+			const boxesClone = boxes.cloneNode(false);
+			const pool = ["❓"]; // Placeholder item while spinning
 
-              boxesClone.addEventListener(
-                  'transitionend',
-                  function () {
-                      this.querySelectorAll('.box').forEach((box, index) => {
-                          box.style.filter = 'blur(0)';
-                          if (index > 0) this.removeChild(box);
-                      });
-                  },
-                  { once: true }
-              );
-          }
+			if (!firstInit) {
+				// Adding transition event listeners to the boxes for animation effects
+				const arr = [];
+				for (let n = 0; n < (groups > 0 ? groups : 1); n++) {
+					arr.push(...items);
+				}
+				pool.push(...shuffle(arr));
 
-          for (let i = pool.length - 1; i >= 0; i--) {
-              const box = document.createElement('div');
-              box.classList.add('box');
-              box.style.width = door.clientWidth + 'px';
-              box.style.height = door.clientHeight + 'px';
-              box.textContent = pool[i];
-              boxesClone.appendChild(box);
-          }
-          boxesClone.style.transitionDuration = `${duration > 0 ? duration : 1}s`;
-          boxesClone.style.transitionTimingFunction = 'cubic-bezier(0.5, 0, 0.5, 1)';
-          boxesClone.style.transform = `translateY(-${door.clientHeight * (pool.length - 1)}px)`;
-          door.replaceChild(boxesClone, boxes);
-      }
-  }
+				boxesClone.addEventListener(
+					"transitionstart",
+					function () {
+						door.dataset.spinned = "1";
+						this.querySelectorAll(".box").forEach((box) => {
+							box.style.filter = "blur(1px)";
+						});
+					},
+					{ once: true }
+				);
 
-  function slowDownAnimation(element, duration) {
-      element.style.transition = `transform ${duration}s ease-in-out`;
-      element.style.transform = 'translateY(0)';
-  }
+				boxesClone.addEventListener(
+					"transitionend",
+					function () {
+						this.querySelectorAll(".box").forEach((box, index) => {
+							box.style.filter = "blur(0)";
+							if (index > 0) this.removeChild(box);
+						});
+					},
+					{ once: true }
+				);
+			}
 
-  function spin() {
-      coinSound.play();
-      init(false, 1, 2);
+			// Creating and appending boxes with items to the door for display
+			for (let i = pool.length - 1; i >= 0; i--) {
+				const box = document.createElement("div");
+				box.classList.add("box");
+				box.style.width = door.clientWidth + "px";
+				box.style.height = door.clientHeight + "px";
+				box.textContent = pool[i];
+				boxesClone.appendChild(box);
+			}
 
-      setTimeout(() => {
-          spinSound.play();
+			// Setting up transition properties for the animation
+			boxesClone.style.transitionDuration = `${
+				duration > 0 ? duration : 1
+			}s`;
+			// ! this is commented by lenddy
+			// boxesClone.style.transitionTimingFunction =
+			// 	"cubic-bezier(0.5, 0, 0.5, 1)";
+			boxesClone.style.transform = `translateY(-${
+				door.clientHeight * (pool.length - 1)
+			}px)`;
+			door.replaceChild(boxesClone, boxes);
+		}
+	}
 
-          for (const door of doors) {
-              const boxes = door.querySelector('.boxes');
-              const duration = parseInt(boxes.style.transitionDuration);
-              boxes.style.transform = 'translateY(0)';
-              new Promise((resolve) => setTimeout(resolve, duration * 100));
-          }
-      }, 500);
-  }
+	// Function to slow down animation by modifying transition properties
+	// ! this is commented by lenddy
+	// function slowDownAnimation(element, duration) {
+	// 	element.style.transition = `transform ${duration}s ease-in-out`;
+	// 	element.style.transform = "translateY(0)";
+	// }
 
-  function shuffle([...arr]) {
-      let m = arr.length;
-      while (m) {
-          const i = Math.floor(Math.random() * m--);
-          [arr[m], arr[i]] = [arr[i], arr[m]];
-      }
-      return arr;
-  }
+	// Function to initiate the spinning of the slot machine
+	function spin() {
+		coinSound.play(); // Playing the coin sound
+		init(false, 1, 2); // Initiating spinning animation
 
-  init();
+		// Delaying the spinning sound to create a sequence effect
+		setTimeout(() => {
+			spinSound.play(); // Playing the spinning sound
 
+			// Looping through each door for the spinning animation effect
+			for (const door of doors) {
+				const boxes = door.querySelector(".boxes");
+				const duration = parseInt(boxes.style.transitionDuration);
+				boxes.style.transform = "translateY(0)";
+				new Promise((resolve) => setTimeout(resolve, duration * 100));
+			}
+		}, 500); // Delay before playing the spinning sound
+	}
+
+	// Function to shuffle the items using the Fisher-Yates algorithm
+	function shuffle([...arr]) {
+		let m = arr.length;
+		while (m) {
+			const i = Math.floor(Math.random() * m--);
+			[arr[m], arr[i]] = [arr[i], arr[m]];
+		}
+		return arr;
+	}
+
+	// Initiating the slot machine on page load
+	init();
 })();
-
-
-
-
