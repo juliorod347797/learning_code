24d037c05510237c80472d0045a3257a8dbe4eec
julio347 <Joshuarodriguez3512@gmail.com>
2023-11-19 02:47:47 -0600

    update

---
+++ 1mystudies/mylecatures/javascript/sidenavfan.js
@@ -0,0 +1,9 @@
+// sidenavfan.js
+
+function toggleNav() {
+    var sidenav = document.querySelector('.sidenav');
+    sidenav.style.width = sidenav.style.width === '250px' ? '0' : '250px';
+}
+
+
+
