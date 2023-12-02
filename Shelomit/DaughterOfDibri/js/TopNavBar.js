8f955ffcc911321bccdcd9f02a60740ee2b3cc7f
julio347 <Joshuarodriguez3512@gmail.com>
2023-12-01 18:26:48 -0600

    update

--- Shelomit/DaughterOfDibri/js/TopNavBar.js
+++ Shelomit/DaughterOfDibri/js/TopNavBar.js
@@ -29,3 +29,4 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Event listener for the 'scroll' event
 window.addEventListener("scroll", handleScroll);
+
---
+++ Shelomit/DaughterOfDibri/js/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+function getTotal(arrayOfNumbers) {
+    var sum = arrayOfNumbers[0];
+    for (var i = 0; i < arrayOfNumbers.length; i++) {
+      sum += arrayOfNumbers[i];
+      console.log("the current sum is " + sum);
+    }
+    console.log("the total sum is " + sum);
+  }
+  
+  getTotal([1, 3, 5]);
+  
